df2fba1acdc792de4db3cad1f010de9f29d6d0a4
Mahad-Ghauri <mahadghauri222@gmail.com>
2024-05-26 15:50:17 +0500

    This enables to keep a check on the checking and checking out of the vehicles in the company by storing the system live time on check in and check out respectively

---
+++ Parking.h
@@ -0,0 +1,189 @@
+#include "Person.h"
+using namespace std;
+
+class parking
+{
+private:
+  static const int max_space = 10;
+  string name[max_space];
+  int employee_id[max_space];
+  time_t parking_Time[max_space];
+  int spots;
+
+public:
+  parking()
+  {
+    spots = 0;
+  }
+
+  bool allot_Spots(string n, int i)
+  {
+    if (spots < max_space)
+    {
+      name[spots] = n;
+      employee_id[spots] = i;
+      parking_Time[spots] = time(0);
+      spots++;
+      return true;
+    }
+    return false;
+  }
+
+  bool dismiss_Spots(int i)
+  {
+    for (int index = 0; index < spots; index++)
+    {
+      if (employee_id[index] == i)
+      {
+        for (int j = index; j < spots - 1; j++)
+        {
+          name[j] = name[j + 1];
+          employee_id[j] = employee_id[j + 1];
+          parking_Time[j] = parking_Time[j + 1];
+        }
+        spots--;
+        return true;
+      }
+    }
+    return false;
+  }
+
+  void check_in()
+  {
+    ofstream checking_in("checkIn_info.txt", ios::app);
+    if (!checking_in.is_open())
+    {
+      cout << "Error opening file!" << endl;
+      return;
+    }
+
+    cout << "Number of parked employees: " << spots << endl;
+    for (int i = 0; i < spots; i++)
+    {
+      cout << "-----------------------------------------------------" << endl;
+      cout << " Employee with id:  " << employee_id[i] << endl;
+      cout << " Name:  " << name[i] << endl;
+      cout << " Checked in at:  " << ctime(&parking_Time[i]) << endl;
+      cout << "-----------------------------------------------------" << endl;
+      checking_in << employee_id[i] << endl
+                  << name[i] << endl;
+    }
+    checking_in.close();
+  }
+
+  void check_out()
+  {
+    cout << "Number of parked employees: " << spots << endl;
+    for (int i = 0; i < spots; i++)
+    {
+      cout << "______________________________________________________" << endl;
+      cout << "| Employee with id: " << setw(20) << left << employee_id[i] << "|" << endl;
+      cout << "| Name            : " << setw(20) << left << name[i] << "|" << endl;
+      cout << "| Checked out at  : " << setw(20) << left << ctime(&parking_Time[i]) << "|" << endl;
+      cout << "-----------------------------------------------------" << endl;
+    }
+
+    ofstream update_file("checkIn_info.txt");
+    if (!update_file.is_open())
+    {
+      cout << "Error opening file!" << endl;
+      return;
+    }
+    for (int i = 0; i < spots; i++)
+    {
+      update_file << employee_id[i] << endl
+                  << name[i] << endl;
+    }
+    update_file.close();
+  }
+};
+
+int main()
+{
+  int id, check_id;
+  string name;
+  bool found = false;
+  parking obj;
+
+a:
+  cout << "Enter the id of the employee: " << endl;
+  cin >> id;
+  cin.ignore();
+  cout << "Enter the name of the employee: " << endl;
+  getline(cin, name);
+
+  char option;
+
+  cout << "Do you want to check in or check out? " << endl;
+  cout << "|------------------------------------------|" << endl;
+  cout << "|      1--> Check In                       |" << endl;
+  cout << "|      2--> Check Out                      |" << endl;
+  cout << "|------------------------------------------|" << endl;
+  option = getch();
+
+  switch (option)
+  {
+  case '1':
+  {
+    ifstream check_existing("checkIn_info.txt");
+
+    while (check_existing >> check_id)
+    {
+      string temp_name;
+      getline(check_existing, temp_name); // Consume the newline character
+      getline(check_existing, temp_name);
+      if (check_id == id)
+      {
+        found = true;
+        break;
+      }
+    }
+    check_existing.close();
+    if (found)
+    {
+      cout << " Employee is already parked." << endl;
+      goto a;
+    }
+    else
+    {
+      obj.allot_Spots(name, id);
+      obj.check_in();
+      goto a;
+    }
+    break;
+  }
+
+  case '2':
+  {
+    ifstream check_existing("checkIn_info.txt");
+
+    while (check_existing >> check_id)
+    {
+      string temp_name;
+      getline(check_existing, temp_name); // Consume the newline character
+      getline(check_existing, temp_name);
+      if (check_id == id)
+      {
+        found = true;
+        break;
+      }
+    }
+    check_existing.close();
+    if (found)
+    {
+      obj.dismiss_Spots(id);
+      obj.check_out();
+    }
+    else
+    {
+      cout << "Employee not found." << endl;
+    }
+    break;
+  }
+  default:
+  {
+    cout << "Select a valid option. " << endl;
+    break;
+  }
+  }
+}
